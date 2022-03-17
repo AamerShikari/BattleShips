31e76bf8e20699c1ca3c278905a7a3fb3fd60c51
AamerShikari <shikariaamer@gmail.com>
2022-03-17 19:08:17 -0400

    Upload ship variant pictures, created setBackground function for redundancies, tested image overlay

--- js/app.js
+++ js/app.js
@@ -23,7 +23,51 @@ compDiv.addEventListener('click', function(e) {
     console.log(e.target)
 })
 
+//PROBLEM - > Need to accommodate to rotating ship divs
+
 let playerGrid = makeRows(playerDiv, 7, 7);
+
+//Two
+playerGrid[0].style.background = "url('pics/Two/TwoLeft.png')";
+playerGrid[1].style.background = "url('pics/Two/TwoRight.png')";
+
+// playerGrid[0].style.transform = "rotate(90deg)";
+
+//ThreeSlick
+playerGrid[7].style.background = "url('pics/ThreeSlick/ThreeSlickLeft.png')";
+playerGrid[8].style.background = "url('pics/ThreeSlick/ThreeSlickMiddle.png')";
+playerGrid[9].style.background = "url('pics/ThreeSlick/ThreeSlickRight.png')";
+
+//ThreeWide
+playerGrid[14].style.background = "url('pics/ThreeWide/ThreeWideLeft.png')";
+playerGrid[15].style.background = "url('pics/ThreeWide/ThreeWideMiddle.png')";
+playerGrid[16].style.background = "url('pics/ThreeWide/ThreeWideRight.png')";
+
+//Four
+playerGrid[21].style.background = "url('pics/Four/FourLeft.png')";
+playerGrid[22].style.background = "url('pics/Four/FourMidLeft.png')";
+playerGrid[23].style.background = "url('pics/Four/FourMidRight.png')";
+playerGrid[24].style.background = "url('pics/Four/FourRight.png')";
+
+//Five
+playerGrid[28].style.background = "url('pics/Five/FiveLeft.png')";
+playerGrid[29].style.background = "url('pics/Five/FiveMidLeft.png')";
+playerGrid[30].style.background = "url('pics/Five/FiveMid.png')";
+playerGrid[31].style.background = "url('pics/Five/FiveMidRight.png')";
+playerGrid[32].style.background = "url('pics/Five/FiveRight.png')";
+
+function setBackground(i, j) {
+    for (let n = i; n <= j; n++){
+        playerGrid[n].style.backgroundSize = "100%"
+        playerGrid[n].style.backgroundRepeat = "no-repeat"
+    }
+}
+setBackground(0, 1);
+setBackground(7,9);
+setBackground(14,16);
+setBackground(21,24);
+setBackground(28,32);
+
 
 let computerGrid = makeRows(compDiv, 7, 7);
 
