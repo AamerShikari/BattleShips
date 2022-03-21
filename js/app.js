40209f73b0042d083405113ea823b68a8f4271bd
AamerShikari <shikariaamer@gmail.com>
2022-03-21 12:07:27 -0400

    Horizontal and Vertical picture addition, player setup, enemy board interactions, bug fixes

--- js/app.js
+++ js/app.js
@@ -25,18 +25,131 @@ Responsibilities:
 - Must run an isGameOver function at the end of every round 
 - 
 */
+let phrase = document.createElement('h2');
+setOfPhrases = ["Select the start index of your ", "Select the end index of your ", "Your all set! Let the battle begin!", "Note: Boat length must match the description, no overlapping, and no diagonals allowed! Try again by entering a start index for a "]
+setOfBoats = ["2-length boat", "3-length boat", "2nd 3-length boat", "4-length boat", "5-length boat"];
+playerBoats = []
+phraseCount = 0
+boatCount = 0
+phrase.innerText = setOfPhrases[0] + setOfBoats[0]
+document.querySelector("#playerBoard").after(phrase)
+let x;
+let y;
+let boatType = [1, 2, 2, 3, 4]
+let playerShips;
 
 playerDiv.addEventListener('click', function(e) {
-    playerGrid.forEach((elem) => {
-        if (elem.innerText === e.target.innerText) {
-            if (elem.id === 'hit') {
-                elem.id = ''
+    
+    if (boatCount === 5) {
+        //Come back to this whent he game has more functionality
+    }
+    if (phraseCount === 0 || phraseCount === 3){
+        x = e.target.innerText
+        phraseCount = 1
+    } else if (phraseCount === 1){
+        y = e.target.innerText
+        if (boatIsValid(x, y, playerBoats, boatType[boatCount])){
+            boatCount++;
+            placeBoat(playerBoats[playerBoats.length - 1], boatType[boatCount], boatCount === 2)
+            if (boatCount === 5){
+                phraseCount = 2
             } else {
-                elem.id = 'hit'
+                phraseCount = 0
             }
+        } else {
+            phraseCount = 3;
         }
-    })
+    }
+    // console.log(boatCount)
+    if (boatCount === 5) {
+        phrase.innerText = setOfPhrases[phraseCount]
+        playerShips = createBoatArr(playerBoats)
+        console.log(playerShips)
+        console.log(playerBoats)
+    } else {
+        phrase.innerText = setOfPhrases[phraseCount] + setOfBoats[boatCount]
+    }
+    
 })
+
+let twoBoatHor = ["url(../pics/Two/Hor/TwoLeft.png", "url(../pics/Two/Hor/TwoRight.png"];
+let twoBoatVert = ["url(../pics/Two/Vert/TwoLeft.png", "url(../pics/Two/Vert/TwoRight.png"]
+let threeSlickHor = ["url(../pics/ThreeSlick/Hor/ThreeSlickLeft.png", "url(../pics/ThreeSlick/Hor/ThreeSlickMiddle.png", "url(../pics/ThreeSlick/Hor/ThreeSlickRight.png"]
+let threeSlickVert = ["url(../pics/ThreeSlick/Vert/ThreeSlickLeft.png", "url(../pics/ThreeSlick/Vert/ThreeSlickMiddle.png", "url(../pics/ThreeSlick/Vert/ThreeSlickRight.png"]
+let threeWideHor = ["url(../pics/ThreeWide/Hor/ThreeWideLeft.png", "url(../pics/ThreeWide/Hor/ThreeWideMiddle.png", "url(../pics/ThreeWide/Hor/ThreeWideRight.png"]
+let threeWideVert = ["url(../pics/ThreeWide/Vert/ThreeWideLeft.png", "url(../pics/ThreeWide/Vert/ThreeWideMiddle.png", "url(../pics/ThreeWide/Vert/ThreeWideRight.png"]
+let fourHor = ["url(../pics/Four/Hor/FourLeft.png", "url(../pics/Four/Hor/FourMidLeft.png", "url(../pics/Four/Hor/FourMidRight.png", "url(../pics/Four/Hor/FourRight.png"]
+let fourVert = ["url(../pics/Four/Vert/FourLeft.png", "url(../pics/Four/Vert/FourMidLeft.png", "url(../pics/Four/Vert/FourMidRight.png", "url(../pics/Four/Vert/FourRight.png"]
+let fiveHor = ["url(../pics/Five/Hor/FiveLeft.png", "url(../pics/Five/Hor/FiveMidLeft.png", "url(../pics/Five/Hor/FiveMid.png", "url(../pics/Five/Hor/FiveMidRight.png", "url(../pics/Five/Hor/FiveRight.png"]
+let fiveVert = ["url(../pics/Five/Vert/FiveLeft.png", "url(../pics/Five/Vert/FiveMidLeft.png", "url(../pics/Five/Vert/FiveMid.png", "url(../pics/Five/Vert/FiveMidRight.png", "url(../pics/Five/Vert/FiveRight.png"]
+let imgArr = [twoBoatHor, twoBoatVert, threeSlickHor, threeSlickVert, threeWideHor, threeWideVert, fourHor, fourVert, fiveHor, fiveVert]
+function placeBoat(boat, type, isSecond) {
+    let index;
+    if (type === 1) {
+        if (boat[4]){
+            index = 0;
+        } else {
+            index = 1
+        }
+    } else if (type === 2){
+        if (isSecond){
+            if (boat[4]){
+                index = 2
+            } else {
+                index = 3
+            }
+        } if (boat[4]){
+            index = 4
+        } else {
+            index = 5
+        }
+    } else if (type === 3){
+        if (boat[4]){
+            index = 6
+        } else {
+            index = 7
+        }
+    } else {
+        if (boat[4]){
+            index = 8
+        } else {
+            index = 9
+        }
+    }
+    let temp = imgArr[index]
+    temp.forEach((elem) => {
+        
+    })
+    
+}
+
+function boatIsValid (x, y, boatArr, len) {
+    let arrOfPos = []
+    let pos1 = convertToPos(x)
+    let pos2 = convertToPos(y)
+    if (boatArr.length === 0 && (y-x === 1 || y-x === 7)){
+        boatArr.push([pos1[0], pos2[0], pos1[1], pos2[1], pos1[0]-pos2[0] === 0])
+        return true;
+    } else if (boatArr.length === 0 && (y-x === -1 || y-x === -7)){
+        boatArr.push([pos1[0], pos2[0], pos1[1], pos2[1], pos1[0]-pos2[0] === 0])
+        return true;
+    } else {
+        if (pos1[0] - pos2[0] !== 0 && pos1[1] - pos2[1] !== 0){
+            return false
+        } else if ((pos1[0] - pos2[0] === 0 && Math.abs(pos1[1] - pos2[1]) !== len) || (Math.abs(pos1[0] - pos2[0]) !== len && pos1[1] - pos2[1] === 0)) {
+            return false
+        } else {
+            if (pos1[0] - pos2[0] > 0 || pos1[1] - pos2[1] > 0){
+                let temp = pos1;
+                pos1 = pos2;
+                pos2 = temp;
+            }
+            // console.log(boatArr)
+            // console.log(`(${pos1[0]}, ${pos1[1]}) (${pos2[0]}, ${pos2[1]}) `)
+            return isntTaken(boatArr, pos1[0], pos2[0], pos1[1], pos2[1], pos1[0]-pos2[0] === 0)
+        }
+    }
+}
 
 compDiv.addEventListener('click', function(e) {
     computerGrid.forEach((elem) => {        
@@ -101,7 +214,7 @@ let computerGrid = makeRows(compDiv, 7, 7);
 
 
 let boatPositions = [];
-let boatType = [1, 2, 2, 3, 4]
+
 function loadCPUBoard () {
     for (let numBoats = 0; numBoats < 5; numBoats++){
         let isHorizontal = Math.floor(Math.random() * 2) % 2 !== 0
@@ -199,17 +312,17 @@ class ship {
 const shipper = new ship("2row", 0, 0, 0, 1)
 //console.log(shipper.isDown())
 
-boatArry = createBoatArr()
-function createBoatArr() {
+boatArry = createBoatArr(boatPositions)
+function createBoatArr(poses) {
     let arr = [];
     let counter = 0
-    boatPositions.forEach((elem) => {
+    poses.forEach((elem) => {
         arr.push(new ship(boatType[counter] + 1, elem[0], elem[2], elem[1], elem[3]))
         counter++
     })
     return arr;
 }
-console.log(boatArry[0])
+// console.log(boatArry)
 
 function isAHit(num) {
     let location = convertToPos(num)
@@ -217,7 +330,6 @@ function isAHit(num) {
     for (let i = 0; i < boatArry.length; i++) {
         for (let j = 0; j < boatArry[i].indexArr.length; j++){
             if (boatArry[i].indexArr[j].x === location[0] && boatArry[i].indexArr[j].y === location[1]){
-                console.log(boatArry[i].indexArr[j]);
                 boatArry[i].indexArr[j].isHit = true;
                 //INSERT DISPLAY ISDOWN BOAT HERE
                 return true
@@ -232,3 +344,21 @@ function convertToPos(index) {
     return [Math.floor((index-1)/7), (index-1) % 7]
 }
 // console.log(convertToPos(14))
+
+//Setup Players Board
+// let playerBoatArr = []
+// let temp = 'kay'
+
+// function runSetup () {
+//     boatType.forEach(elem => {
+//         let prompt = document.createElement('p')
+        
+//         prompt = `Hello! Please select the start index! ${temp}`
+//         document.querySelector('h1').after(prompt)
+//         playerDiv.addEventListener('click', (e) => {
+//             console.log(e.target)
+//             temp = e.target.innerText
+//         })
+//     })
+// }
+// runSetup();
